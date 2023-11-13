df2b9d435592273ecadab1976bf6ea07a94cb9d0
leehjhjhj <tsukiakarii@naver.com>
2023-11-13 11:34:51 +0900

    feat: 404, 400 포함 2차배포

--- change_state.py
+++ change_state.py
@@ -10,8 +10,8 @@ def change_state(event):
             host=os.environ.get('DB_HOST'),
             user=os.environ.get('DB_USER'),
             password=os.environ.get('DB_PASSWORD'),
-            port=os.environ.get('DB_PORT'),
-            db=3306,
+            port=3306,
+            db=os.environ.get('DB_NAME'),
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor,
         )
@@ -20,21 +20,31 @@ def change_state(event):
     report_id = event['reportId']
     try:
         with conn.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM sports_live.comments WHERE id = {comment_id};")
+            if cursor.rowcount == 0:
+                status_code = '404'
+                return status_code
+            cursor.execute(f"SELECT * FROM sports_live.reports WHERE id = {report_id};")
+            if cursor.rowcount == 0:
+                status_code = '404'
+                return status_code
+
             if check_badword(target):
                 cursor.execute(f"UPDATE sports_live.comments SET is_blocked = 1 WHERE id = {comment_id};")
+                if cursor.rowcount == 0:
+                    status_code = '400'
+                    return status_code
                 report_state = 'VALID'
-                response = '검거? 완료.'
             else: 
                 report_state = 'PENDING'
-                response = '보류'
 
             cursor.execute(f"UPDATE sports_live.reports SET state = '{report_state}' WHERE id = {report_id};")
-            result = cursor.fetchall()
-            if result:
-                for row in result:
-                    logging.info(row)
-            conn.commit() 
-            return response
+            if cursor.rowcount == 0:
+                status_code = '400'
+                return status_code
+            conn.commit()
+            status_code = '200'
+            return status_code
         
     except Exception as e:
         logging.error(f'DB 에러 발생: {e}')
--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,7 @@
 from change_state import change_state
 
 def lambda_handler(event, context):
-    response = change_state(event)
+    status_code = change_state(event)
     return {
-        'statusCode': 200,
-        'response': response
+        'statusCode': status_code,
     }
